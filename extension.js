e1f636eaee2cf482bf041a6f3976bd84082f36fd
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-06-23 03:53:23 +0200

    first commit

---
+++ extension.js
@@ -0,0 +1,85 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+import Clutter from 'gi://Clutter';
+// import AppDisplay from 'resource:///org/gnome/shell/ui/appDisplay.js'
+
+export default class PlainExampleExtension extends Extension {
+    enable() {
+        const appDisplay = Main.overview._overview._controls._appDisplay;
+        
+        // log('Overview grid:', appDisplay, appDisplay._grid);
+        log("I'm okay!");
+        
+        const appGrid = appDisplay._grid;
+        const swipeTracker = appGrid._delegate._swipeTracker;
+
+        const children = appGrid.get_children();
+
+        for (let i = 0; i < children.length; ++i) {
+            // appIcon = r(196);
+            // console.debug('appIcon', appIcon);
+            console.debug(i);
+            const appIcon = children[i];
+
+            let startPos = 0;
+            let lastPos = 0;
+
+            appIcon.connect('touch-event', (actor, event) => {
+                const type = event.type();
+                const coords = event.get_coords();
+                const time = event.get_time();
+                const pos = coords[0];
+                // const delta = appGrid._currentPage - (pos - startPos);
+                const distance = appGrid._delegate._adjustment.page_size;
+                const progress = appGrid._currentPage - (pos - startPos) / appGrid._delegate._adjustment.page_size;
+                
+                console.debug(event, pos, time, progress);
+                
+                switch (type) {
+                    case Clutter.EventType.TOUCH_BEGIN:
+                    appGrid._delegate._swipeTracker._beginTouchSwipe(this, time, coords[0], coords[1]);
+                    startPos = pos;
+                    lastPos = startPos;
+
+                    break;
+                case Clutter.EventType.TOUCH_UPDATE:
+                    appIcon._delegate._removeMenuTimeout();
+
+                    const delta = -(pos - lastPos);
+                    lastPos = pos;
+
+                    appGrid._delegate._swipeTracker._updateGesture(this, time, delta, distance);
+
+                    // swipeTracker.emit('update', progress);
+                    break;
+                case Clutter.EventType.TOUCH_END:
+                    // swipeTracker.emit('end', 3, Math.round(progress));
+                    appGrid._delegate._swipeTracker._endTouchGesture(this, time, distance);
+                    return true;
+                    break;
+                }
+            });
+        }
+    }
+
+    disable() {
+    }
+}
